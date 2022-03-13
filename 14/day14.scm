ebfe5295997cbac819059648a382f4d6ae72b014
Sjoerd Dost <deosjr@gmail.com>
2022-03-13 16:36:12 +0100

    day 14 done

--- 14/day14.scm
+++ 14/day14.scm
@@ -5,7 +5,6 @@
 (load "10/knothash.scm")
 
 (define input "oundnydw")
-;;(define input "flqrgnkx")
 
 ;; where n is an 8-bit unsigned integer
 (define (ones n)
@@ -34,15 +33,18 @@
 
 (define coords (make-hashtable equal-hash equal?))
 
+(define coordlist '())
 (define (store-coord x y)
-  (hashtable-set! coords (make-coord x y) #t))
+  (let ((c (make-coord x y)))
+  (set! coordlist (cons c coordlist))
+  (hashtable-set! coords c #t)))
 
 (define (store-ones num x y)
   (letrec ((count-ones 
     (lambda (n e)
       (let ((expo (expt 2 e)))
         (cond
-          [(= e 0) (if (div n 1) (store-coord (+ x 7) y))]
+          [(= e 0) (if (= n 1) (store-coord (+ x 7) y))]
           [(<= expo n) (store-coord (+ x (- 7 e)) y) (count-ones (- n expo) (- e 1))]
           [(> expo n) (count-ones n (- e 1))]
         )))))
@@ -57,4 +59,26 @@
 (define (khash k) (knothash (knothashkey (key k))))
 (map (lambda (i) (store-for-key (khash i) i)) (iota 128))
 
-(for-each (lambda (x) (write-part1 (hashtable-ref coords (make-coord x 8) #f))) (iota 16))
+(define (coord? c)
+  (hashtable-ref coords c #f))
+
+(define (neighbours c)
+  (let ((x (cadr c)) (y (caddr c)))
+    (list
+      (make-coord (- x 1) y)
+      (make-coord (+ x 1) y)
+      (make-coord x (- y 1))
+      (make-coord x (+ y 1))
+  )))
+
+(define (remove-region c)
+  (hashtable-delete! coords c)
+  (map (lambda (x) (remove-region x)) (filter coord? (neighbours c))))
+
+(define p2
+  (do ((clist coordlist (filter coord? clist)) (regions 0 (+ 1 regions)))
+    ((null? clist) regions)
+    (remove-region (car clist))
+  ))
+
+(write-part2 p2)
