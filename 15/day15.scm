52d1877a2ba7e50b01dc9183ccd3af3fbee2cf49
Sjoerd Dost <deosjr@gmail.com>
2022-05-20 16:38:56 +0200

    day15 part 2

--- 15/day15.scm
+++ 15/day15.scm
@@ -3,16 +3,14 @@
 
 (define gena 699)
 (define genb 124)
-;(define gena 65)
-;(define genb 8921)
+
+(define (next-until-mod g factor m)
+  (let ((x (modulo (* g factor) 2147483647)))
+    (if (= (modulo x m) 0) x (next-until-mod x factor m))))
 
 (define (next generators)
   (let ((a (car generators)) (b (cdr generators)))
-    (cons (modulo (* a 16807) 2147483647)
-        (modulo (* b 48271) 2147483647))))
-
-(write-part1 (cons gena genb))
-(write-part1 (next (cons gena genb)))
+    (cons (next-until-mod a 16807 1) (next-until-mod b 48271 1))))
 
 ; 65536 is 2**16
 (define (judge? generators)
@@ -20,11 +18,17 @@
     (= (modulo a 65536) (modulo b 65536))
   ))
 
-(define (p1 i gens)
+(define (p1 i gens f)
   (cond
     [(= i 0) 0]
-    [else (let ((ngens (next gens)) (ni (- i 1)))
-      (cond [(judge? ngens) (+ 1 (p1 ni ngens))] [else (p1 ni ngens)]))
+    [else (let ((ngens (f gens)) (ni (- i 1)))
+      (cond [(judge? ngens) (+ 1 (p1 ni ngens f))] [else (p1 ni ngens f)]))
     ]))
 
-(write-part1 (p1 40000000 (cons gena genb)))
+(write-part1 (p1 40000000 (cons gena genb) next))
+
+(define (next2 generators)
+  (let ((a (car generators)) (b (cdr generators)))
+    (cons (next-until-mod a 16807 4) (next-until-mod b 48271 8))))
+
+(write-part2 (p1 5000000 (cons gena genb) next2))
