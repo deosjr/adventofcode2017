14228eb56041be12f92a44e52af5f2011a89e248
Sjoerd Dost <deosjr@gmail.com>
2022-05-21 15:57:37 +0200

    day17 part 2

--- 17/day17.scm
+++ 17/day17.scm
@@ -2,9 +2,7 @@
 (load "lib/lib.scm")
 
 (define input 303)
-;(define input 3)
 
-; todo cons
 (define state '(0))
 (define position 0)
 
@@ -18,3 +16,18 @@
   ) (iota 2017))
 
 (write-part1 (list-ref state (+ 1 position)))
+
+; once more but without actually creating the list
+(set! state 1) ; only length of the list this time
+(set! position 0)
+(define (step) (set! position (modulo (+ position (modulo input state)) state)))
+
+(define ans 0)
+(for-each (lambda (i) 
+    (step)
+    (if (= position 0) (set! ans state))
+    (set! state (+ state 1))
+    (set! position (+ position 1))
+  ) (iota 50000000))
+
+(write-part2 ans)
