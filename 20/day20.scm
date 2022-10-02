18f261c095981a44dc82c51733880e6ffa29bfd8
Sjoerd Dost <deosjr@gmail.com>
2022-10-02 08:27:30 +0200

    rewrite day20 part 1

--- 20/day20.scm
+++ 20/day20.scm
@@ -1,13 +1,11 @@
 #! /usr/bin/scheme --script
 (load "lib/lib.scm")
 
-(define input (filehandle 20))
-
+; TODO move to lib
 ; (sscanf "p=<1,2,3>" "p=<%d,%d,%d>") -> (1 2 3)
 ; silently fails and returns whatever succeeded so far in a list
 (define (sscanf str fmt)
-  (define strhandle (open-input-string str))
-  (define fmthandle (open-input-string fmt))
+  (define strhandle (open-input-string str)) (define fmthandle (open-input-string fmt))
   ; parse a string until space
   (define (scanstr)
     (define (scanstr-rec)
@@ -43,35 +41,41 @@
 
 (define (parseline line)
   (let ((numbers (sscanf line "p=<%d,%d,%d>, v=<%d,%d,%d>, a=<%d,%d,%d>")))
-    (apply (lambda (p0 p1 p2 v0 v1 v2 a0 a1 a2 . x)
-    (list (list p0 p1 p2) (list v0 v1 v2) (list a0 a1 a2))) numbers)))
+    (let-values (( (p0 p1 p2 v0 v1 v2 a0 a1 a2) (apply values numbers) ))
+      (list (list p0 p1 p2) (list v0 v1 v2) (list a0 a1 a2)))))
 
 (define (vector-len v)
-  (let ((v0 (car v)) (v1 (cadr v)) (v2 (caddr v)))
+  (let-values (( (v0 v1 v2) (apply values v)))
     (+ (abs v0) (abs v1) (abs v2))))
 
 (define (vector-add p q)
-  (list (+ (car p) (car q)) (+ (cadr p) (cadr q)) (+ (caddr p) (caddr q))))
+  (let-values (( (p0 p1 p2) (apply values p))
+               ( (q0 q1 q2) (apply values q)))
+    (list (+ p0 q0) (+ p1 q1) (+ p2 q2))))
 
 (define (vector-mul s v)
-  (let ((v0 (car v)) (v1 (cadr v)) (v2 (caddr v)))
+  (let-values (( (v0 v1 v2) (apply values v)))
     (list (* v0 s) (* v1 s) (* v2 s))))
+
+(define (parsed-input)
+  (define (parse-input input)
+    (let ((line (get-line input)))
+      (if (eq? #!eof line) '()
+        (cons (parseline line) (parse-input input)))))
+  (parse-input (filehandle 20)))
 
 ; part 1 is finding the particle with smallest acceleration
 ; and accounting for starting positions some others might be closer at start
-(define p1 (cons 0 9999999))
+(define (p1)
+  (define (p1-rec particles idx ans)
+    (if (null? particles) (car ans)
+      (let ((particle (car particles)))
+        (let-values (( (p v a) (apply values particle)))
+          (let* ((sum (vector-add (vector-add p (vector-mul 1000 v)) (vector-mul (/ (* 1000 1001) 2) a)))
+                 (len (vector-len sum)))
+            (if (< len (cdr ans))
+              (p1-rec (cdr particles) (+ 1 idx) (cons idx len))
+              (p1-rec (cdr particles) (+ 1 idx) ans)))))))
+  (p1-rec (parsed-input) 0 (cons 0 99999999)))
 
-(do ((line (get-line input) (get-line input))
-     (linenum 0 (+ 1 linenum)))
-  ((eq? #!eof line))
-  (let* ((parsed (parseline line))
-         (p (car parsed))
-         (v (cadr parsed))
-         (a (caddr parsed))
-         (sum (vector-add (vector-add p (vector-mul 1000 v)) (vector-mul (/ (* 1000 1001) 2) a)))
-         (len (vector-len sum))
-         )
-    (if (< len (cdr p1))
-      (set! p1 (cons linenum len)))))
-
-(write-part1 (car p1))
+(write-part1 (p1))
